fafc7ef81194d091ee9a812adaf4bc376e5de2f9
rzayevruslan540 <rzayevruslan540@gmail.com>
2025-10-27 22:12:00 +0400

    LEVEL 1 TASK 1

---
+++ Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string ad;
+    cout << "Adinizi daxil edin: ";
+    cin >> ad;
+    cout << "Salam, " << ad << "!" << endl;
+    return 0;
+}
